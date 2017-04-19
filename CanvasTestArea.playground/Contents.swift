eefbf45be2d66c0377c7a6ae040da425c2623175
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2017-04-19 09:28:27 -0400

    it will draw an axiom

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -18,17 +18,63 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 500, height: 500)
 
 // View the current state of the canvas
 canvas
 
+let angle : Degrees = 60
+let distance = 100
+let iterations = 5
+let axiom = "F++F++F"
+var startX = 10
+var startY = 10
+
 // Draw the axes
 canvas.drawAxes()
 
+canvas.translate(byX: startX, byY: startY)
+
+startX = 0
+startY = 0
+canvas.saveState()
+
+func drawAxiom(axiom: String) {
+
+for char in axiom.characters {
+    
+    if char == "F" {
+        
+        canvas.drawLine(fromX: startX, fromY: startY, toX: distance, toY: startY)
+        canvas.translate(byX: distance, byY: 0)
+       // canvas.drawAxes()
+        
+    } else if char == "+" {
+        
+        canvas.rotate(by: angle)
+        //canvas.drawAxes()
+        
+    } else if char == "-" {
+        
+        canvas.rotate(by: 0 - angle)
+        //canvas.drawAxes()
+        
+    } else {
+        
+        print("axiom character \(char) not understood")
+    }
+    //canvas.drawAxes()
+    
+}
+canvas.restoreState()
+}
+
+for iteration in 0...iterations {
+
+    
 // Add code below...
-
-
+    
+}
 /*:
  
  ## To see output
